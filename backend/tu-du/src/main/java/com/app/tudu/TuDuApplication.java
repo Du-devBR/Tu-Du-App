91aebcd6e7eba6c0987c1bef2ceb95e88200178f
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-04-23 19:50:40 -0300

    estrutura backend inicializado com java springboot

---
+++ backend/tu-du/src/main/java/com/app/tudu/TuDuApplication.java
@@ -0,0 +1,13 @@
+package com.app.tudu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuDuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuDuApplication.class, args);
+	}
+
+}
---
+++ backend/tu-du/src/test/java/com/app/tudu/TuDuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tudu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuDuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
