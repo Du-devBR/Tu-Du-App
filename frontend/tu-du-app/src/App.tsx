8d4090ecbc8529b0fb80c1955bd923f2db252476
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-07-02 19:40:01 -0300

    chore: condigurado .env para receber a url base da api

--- frontend/tu-du-app/src/App.tsx
+++ frontend/tu-du-app/src/App.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from "react"
+
+import { urlApi } from "./service/api"
+import axios from "axios";
 
 
 export function App() {
 
+  const [teste, setTeste] = useState([])
+
+  // useEffect(() => {
+
+
+  //   fetch(`http://localhost:8080/api/user`, {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setTeste(data);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYW1pbGFAdGVzdGUuY29tIiwiaXNzIjoidHVkdSIsIm5vbWUiOiJDYW1pbGEiLCJpZCI6MSwic29icmVub21lIjoiQW5hbmlhcyIsImV4cCI6MTY4ODM0OTc3NH0.ljFT-TeiEeTtLKbS3iFmYX_0Q2x1xhYdgM8XodYuE6g';
+
+    axios.get(`${urlApi}/api/users/1/tasks`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        const data = response.data;
+        setTeste(data);
+      })
+      .catch((error) => {
+        // Tratar erros
+        console.error(error);
+      });
+  }, []);
+
+
   return (
     <>
     <h1>Hello word</h1>
+    {
+      teste.map((t) => (
+        <span key={t.id}>{JSON.stringify(t.title)}</span>
+      ))
+    }
     </>
   )
 }
---
+++ frontend/tu-du-app/src/service/api.ts
@@ -0,0 +1,1 @@
+export const urlApi = import.meta.env.VITE_REACT_APP_API_URL;
