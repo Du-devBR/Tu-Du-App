b0209e82a381acbf833a1333667e500c7f9119cc
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-07-27 23:05:38 -0300

    refactor: corrigido logica get para filtrar co base no status

--- frontend/tu-du-app/src/pages/home/index.tsx
+++ frontend/tu-du-app/src/pages/home/index.tsx
@@ -6,21 +6,52 @@ import { fetchGetTasks } from "../../redux/reducer/tasks/actionGet"
 export function Home(){
 
   const dispatch = useDispatch()
-  const {data, error, loading} = useSelector((store: IRootState) => store.tasks)
+  const {error, loading, taskStatus} = useSelector((store: IRootState) => store.tasks)
 
   useEffect(() => {
     dispatch<any>(fetchGetTasks())
 
   }, [dispatch])
 
-  console.log("dados: " + JSON.stringify(data))
+  console.log("dados: " + JSON.stringify(taskStatus))
   return(
     <div>
-      {
+      <div>
+        <h2>Status: Aberto</h2>
+        {
+          taskStatus.ABERTO?.map((task: ITaskData) => (
+            <div key={task.title}>
+              <p>{task.title}</p>
+            </div>
+          ))
+        }
+      </div>
+
+      <div>
+        <h2>Status: Iniciado</h2>
+        {
+          taskStatus.INICIADO?.map((task: ITaskData) => (
+            <div key={task.title}>
+              <p>{task.title}</p>
+            </div>
+          ))
+        }
+      </div>
+      <div>
+        <h2>Status: Concluido</h2>
+        {
+          taskStatus.CONCLUIDO?.map((task: ITaskData) => (
+            <div key={task.title}>
+              <p>{task.title}</p>
+            </div>
+          ))
+        }
+      </div>
+      {/* {
         data?.map((task: ITaskData) => (
           <p>{task.title}</p>
         ))
-      }
+      } */}
       {
         loading ? (
           <p>...carregando</p>
--- frontend/tu-du-app/src/redux/reducer/tasks/reducer.ts
+++ frontend/tu-du-app/src/redux/reducer/tasks/reducer.ts
@@ -2,23 +2,40 @@ import { Action } from "./interface-action";
 
 export interface ITasksState {
   loading: boolean | null;
-  data: [] | null;
+  data: ITaskData[] | null;
   error: boolean | null;
+  taskStatus: {
+    ABERTO: ITaskData[] | null,
+    INICIADO: ITaskData[] | null,
+    CONCLUIDO: ITaskData[] | null
+  }
 }
 
-const INITIAL_STATE = {
+const INITIAL_STATE: ITasksState = {
   loading: false,
-  data: [],
   error: false,
+  data: [],
+  taskStatus: {
+    ABERTO: [],
+    INICIADO: [],
+    CONCLUIDO: []
+  }
 };
 
 
 export function TasksReducer(state=INITIAL_STATE, action: Action){
   switch(action.type){
     case "GET_TASKS_SUCCESS":
+      const tasks: ITaskData[] = Array.isArray(action.payload) ? action.payload : [action.payload];
+      const taskStatus = {
+        ABERTO: tasks.filter(task => task.statusTask === 'ABERTO'),
+        INICIADO: tasks.filter(task => task.statusTask === 'INICIADO'),
+        CONCLUIDO: tasks.filter(task => task.statusTask === 'CONCLUIDO'),
+      };
       return{
         ...state,
-        data: action.payload,
+        data: tasks,
+        taskStatus,
         error: false,
         loading: false,
       }
